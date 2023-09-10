adbd3171b270ce23cd6d1c037312387b44849990
Shae Smith <sbmith86@gmailcom>
2023-09-10 12:55:31 -0400

    Create simple logic for one round of play - does NOT include or make bonus round considerations~

--- scoreboard/scoreboard.js
+++ scoreboard/scoreboard.js
@@ -1,65 +1,99 @@
-const fakeData = {
-    "rounds":[
-        {
-            'made_shots': ['green1', 'gray2', 'red2'],
-            'attempted_shots': ['green1', 'gray2', 'blue2', 'red2']
-        },
-        // # Round 2
-        {
-            'made_shots': ['green1', 'yellow1', 'gray2', 'blue1'],
-            'attempted_shots': ['green1', 'yellow1', 'gray2', 'blue1', 'red2']
-        },
-        // # Round 3
-        {
-            'made_shots': ['green1', 'yellow1', 'blue2', 'red1', 'blue2', 'gray2', 'gray1', 'red2', 'blue1'],
-            'attempted_shots':  ['green1', 'yellow1', 'blue2', 'red1', 'blue2', 'gray2', 'gray1', 'red2', 'blue1'],
-            'made_bonus_shots': ['green1', 'yellow1', 'gray2']
 
-        },
-        // # Round 4
-        {
-            'made_shots': ['green1', 'yellow1', 'blue2', 'red2'],
-            'attempted_shots': ['green1', 'yellow1', 'blue2', 'red2']
-        },
-        // # Round 5
-        {
-            'made_shots': ['green1', 'yellow1'],
-            'attempted_shots': ['green1', 'yellow1', 'gray2', 'blue2', 'red2']
-        },
-        // # Round 6
-        {
-            'made_shots': ['red2', 'green1', 'blue1', 'red2', 'red1'],
-            'attempted_shots': [ 'red2', 'green1', 'blue1', 'red2' ,'red1', 'green1']
-        },
-        // # Round 7
-        {
-            'made_shots': ['green1', 'yellow1', 'gray2', 'blue1', 'red1'],
-            'attempted_shots': ['green1', 'yellow1', 'gray2', 'blue1', 'red1']
-        },
-        // # Round 8
-        {
-            'made_shots': ['green1', 'yellow1', 'gray2'],
-            'attempted_shots': ['green1', 'yellow1', 'gray2', 'blue1', 'red2']
-        },
-        // # Round 9
-        {
-            'made_shots': ['green1', 'yellow1', 'gray2', 'blue2'],
-            'attempted_shots': ['green1', 'yellow1', 'gray2', 'blue2']
-        },
-        // # Round 10 (Final Round)
-        {
-            'made_shots': ['green1', 'yellow1', 'gray1', 'blue2', 'red2'],
-            'attempted_shots': ['green1', 'yellow1', 'gray1', 'blue2', 'red2'],
-        },
-        ]
+const data = require('./FakeHotshotData.json');
+const fs = require('fs');
+
+const _SCORING = {
+    "green" : 5,
+    "yellow": 4,
+    "gray": 3,
+    "blue": 2,
+    "red": 1
+};
+
+const determineMissedShots = (madeShots, attemptedShots) => {
+    return attemptedShots.filter(shot => !madeShots.includes(shot));
+}
+
+const redShots = (missedShots, madeShots) => {
+    let missedRedShots = 0;
+    let madeRedShots = 0;
+    let deductScore = 0;
+
+    missedShots.forEach(shot => {
+        if(shot === "red1" || shot === "red2") {
+            deductScore = deductScore + 2;
+            missedRedShots++;
+        }
+    })
+
+    madeShots.forEach(shot => {
+        if(shot === "red1" || shot === "red2") {
+            madeRedShots++;
+        }
+    })
+
+    return {
+        "missedRedShots": missedRedShots,
+        "madeRedShots": madeRedShots,
+        "deduct": deductScore
     }
+}
 
-const rounds = JSON.parse(JSON.stringify(fakeData));
+const createDescriptiveRoundMap = (rounds, descriptiveRoundMap) => {
+    let cumalativeScore = 0;
 
-const fakeFn = () => {
-    return [1,2,3,4,5,6,7,8,9,10];
+    rounds.forEach((round, index) => {
+
+        const countMadeShots = round.made_shots.length;
+        const countAttemptedShots = round.attempted_shots.length;
+        const totalRoundShots = countAttemptedShots;
+
+        // Made shots per spot color
+        const madeShotCounts = new Map();
+        let madeShotScore = 0;
+
+        round.made_shots.forEach(shot => {
+            const spotNumber = shot.slice(-1);
+            const spotColor = shot.substring(0, shot.indexOf(spotNumber));
+
+            madeShotCounts.set(spotColor, (madeShotCounts.get(spotColor) || 0) + 1);
+            madeShotScore = madeShotScore + _SCORING[spotColor];
+        });
+
+        // 2 points are deducted for missed red shots. No points deducted for missed bonus red shots.
+        const allMissedShots = determineMissedShots(round.made_shots, round.attempted_shots);
+        const redShotCounts = redShots(allMissedShots, round.made_shots);
+
+        if (redShotCounts.madeRedShots <= 2) {
+            cumalativeScore = cumalativeScore + madeShotScore - redShotCounts.deduct;
+        } else {
+            cumalativeScore = cumalativeScore + 0;
+        }
+        // cumalativeScore = cumalativeScore + madeShotScore - missedRedShots.deduct;
+        descriptiveRoundMap.set(index + 1, {
+            "totalRoundShots": totalRoundShots,
+            "madeShotCountsByColor" : madeShotCounts,
+            "madeShotScore": madeShotScore,
+            "cumalativeScore": cumalativeScore,
+            "allMissedShots": allMissedShots
+        });
+    });
+    // console.log(descriptiveRoundMap);
+    return descriptiveRoundMap;
+};
+
+const createScoreboard = () => {
+    const fakeGameData = data;
+    const rounds = JSON.parse(JSON.stringify(fakeGameData)).data;
+
+    const descriptiveRoundMap = new Map();
+    createDescriptiveRoundMap(rounds, descriptiveRoundMap);
+    return descriptiveRoundMap;
 }
 
 module.exports = {
-    fakeFn
+    createScoreboard,
+    determineMissedShots,
+    redShots,
+    createDescriptiveRoundMap
 };
--- scoreboard/scoreboard.test.js
+++ scoreboard/scoreboard.test.js
@@ -1,7 +1,143 @@
-const { fakeFn } = require("./scoreboard");
+const Scoreboard = require("./scoreboard");
 
-describe('My Test Suite', () => {
-    it('My Test Case', () => {
-      expect(true).toEqual(true);
+// module.exports = {
+//     createScoreboard,
+//     determineMissedShots,
+//     redShots,
+//     createDescriptiveRoundMap
+// };
+
+describe('Hotshot Scoreboard', () => {
+    it('correctly counts all missed shots', () => {
+        const numMissedShots = Scoreboard.determineMissedShots(['green1', 'gray2', 'red2'], ['green1', 'gray2', 'blue2', 'red2']);
+
+        expect(numMissedShots.length).toEqual(1);
+    });
+
+    it('correctly maps out information about red shots made during a round. ', () => {
+        const round = {
+            'made_shots': ['green1', 'yellow1', 'gray2', 'blue1'],
+            'attempted_shots': ['green1', 'yellow1', 'gray2', 'blue1', 'red2']
+        };
+
+        const numMissedShots = Scoreboard.determineMissedShots(round.made_shots, round.attempted_shots);
+        const redShots = Scoreboard.redShots(numMissedShots, round.made_shots);
+
+        expect(redShots.missedRedShots).toEqual(1);
+        expect(redShots.madeRedShots).toEqual(0);
+        expect(redShots.deduct).toEqual(2);
+    });
+
+    it('correctly creates an object with computed data about each round in a game', () => {
+        const descriptiveGame = Scoreboard.createScoreboard();
+        const madeShotCountsByColorMap = new Map();
+        madeShotCountsByColorMap.set('green', 1);
+        madeShotCountsByColorMap.set('yellow', 1);
+        madeShotCountsByColorMap.set('blue', 3);
+        madeShotCountsByColorMap.set('red', 2);
+        madeShotCountsByColorMap.set('gray', 2);
+       // Check that it mapped a single round correctly.
+        const roundThree = descriptiveGame.get(3);
+        // console.log(roundThree);
+        expect(roundThree.totalRoundShots).toEqual(9);
+        expect(roundThree.madeShotCountsByColor).toEqual(madeShotCountsByColorMap);
+        expect(roundThree.madeShotScore).toEqual(23);
+        expect(roundThree.cumalativeScore).toEqual(44); // TO BE UPDATED
+        expect(roundThree.allMissedShots.length).toEqual(0);
+
+
+       /*
+       * Map(10) {
+      1 => {
+        totalRoundShots: 4,
+        madeShotCountsByColor: Map(3) { 'green' => 1, 'gray' => 1, 'red' => 1 },
+        madeShotScore: 9,
+        cumalativeScore: 9,
+        allMissedShots: [ 'blue2' ]
+      },
+      2 => {
+        totalRoundShots: 5,
+        madeShotCountsByColor: Map(4) { 'green' => 1, 'yellow' => 1, 'gray' => 1, 'blue' => 1 },
+        madeShotScore: 14,
+        cumalativeScore: 21,
+        allMissedShots: [ 'red2' ]
+      },
+      3 => {
+        totalRoundShots: 9,
+        madeShotCountsByColor: Map(5) {
+          'green' => 1,
+          'yellow' => 1,
+          'blue' => 3,
+          'red' => 2,
+          'gray' => 2
+        },
+        madeShotScore: 23,
+        cumalativeScore: 44,
+        allMissedShots: []
+      },
+      4 => {
+        totalRoundShots: 4,
+        madeShotCountsByColor: Map(4) { 'green' => 1, 'yellow' => 1, 'blue' => 1, 'red' => 1 },
+        madeShotScore: 12,
+        cumalativeScore: 56,
+        allMissedShots: []
+      },
+      5 => {
+        totalRoundShots: 5,
+        madeShotCountsByColor: Map(2) { 'green' => 1, 'yellow' => 1 },
+        madeShotScore: 9,
+        cumalativeScore: 63,
+        allMissedShots: [ 'gray2', 'blue2', 'red2' ]
+      },
+      6 => {
+        totalRoundShots: 6,
+        madeShotCountsByColor: Map(3) { 'red' => 3, 'green' => 1, 'blue' => 1 },
+        madeShotScore: 10,
+        cumalativeScore: 63,
+        allMissedShots: []
+      },
+      7 => {
+        totalRoundShots: 5,
+        madeShotCountsByColor: Map(5) {
+          'green' => 1,
+          'yellow' => 1,
+          'gray' => 1,
+          'blue' => 1,
+          'red' => 1
+        },
+        madeShotScore: 15,
+        cumalativeScore: 78,
+        allMissedShots: []
+      },
+      8 => {
+        totalRoundShots: 5,
+        madeShotCountsByColor: Map(3) { 'green' => 1, 'yellow' => 1, 'gray' => 1 },
+        madeShotScore: 12,
+        cumalativeScore: 88,
+        allMissedShots: [ 'blue1', 'red2' ]
+      },
+      9 => {
+        totalRoundShots: 4,
+        madeShotCountsByColor: Map(4) { 'green' => 1, 'yellow' => 1, 'gray' => 1, 'blue' => 1 },
+        madeShotScore: 14,
+        cumalativeScore: 102,
+        allMissedShots: []
+      },
+      10 => {
+        totalRoundShots: 5,
+        madeShotCountsByColor: Map(5) {
+          'green' => 1,
+          'yellow' => 1,
+          'gray' => 1,
+          'blue' => 1,
+          'red' => 1
+        },
+        madeShotScore: 15,
+        cumalativeScore: 117,
+        allMissedShots: []
+      }
+    }
+
+       */
     });
   });
